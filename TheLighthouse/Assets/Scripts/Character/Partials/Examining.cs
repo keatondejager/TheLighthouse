d3e0cb63f57d3137070d5762506afaf1ec742d22
Keaton de Jager <keatondejager@gmail.com>
2019-08-25 13:06:51 +0200

    Camera follow, text mesh pro import and examining all done. Just need to do narrative

--- TheLighthouse/Assets/Scripts/Character/Partials/Examining.cs
+++ TheLighthouse/Assets/Scripts/Character/Partials/Examining.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 //Examining
 namespace PlayerManager
@@ -20,7 +21,8 @@ namespace PlayerManager
 
                 [SerializeField] protected Transform _refPoint;
                 [SerializeField] protected GameObject _ExamineParent;
-
+                [SerializeField] protected TMP_Text _objectName;
+                [SerializeField] protected TMP_Text _objectDescription;
                 private InteractableObject _activeExamine;
 
                 public struct InteractableObject {
@@ -35,6 +37,7 @@ namespace PlayerManager
             [Header("Examine Controls")]
                 [SerializeField] protected float _examineZoom;
                 [SerializeField] protected Vector2 _examineRotation;
+                [Range(1f, 2.5f)][SerializeField] protected float _MaxZoom = 1f;
            
 
             #region Methods
@@ -46,6 +49,9 @@ namespace PlayerManager
                 _controls.Examine.Enable();
 
                 Obj.mesh.position = _refPoint.position;
+
+                _objectName.text = Obj.name;
+                _objectDescription.text = Obj.description;
 
                 _activeExamine = Obj;
 
@@ -68,9 +74,14 @@ namespace PlayerManager
                 if (!isExamining) {
                     return;
                 }
-
+                
                 _activeExamine.mesh.position += -Vector3.forward * _examineZoom / 100f * Time.deltaTime;
-                _activeExamine.mesh.Rotate(new Vector3(_examineRotation.y, _examineRotation.x, 0) * 25f * Time.deltaTime, Space.World);
+                if (_activeExamine.mesh.position.z > _refPoint.position.z + _MaxZoom) {
+                    _activeExamine.mesh.position = _refPoint.position + Vector3.forward * _MaxZoom;
+                } else if (_activeExamine.mesh.position.z < _refPoint.position.z - _MaxZoom) {
+                    _activeExamine.mesh.position = _refPoint.position - Vector3.forward * _MaxZoom;
+                }
+                _activeExamine.mesh.Rotate(new Vector3(_examineRotation.y, _examineRotation.x, 0) * 50f * Time.deltaTime, Space.World);
             }
 
             #endregion
