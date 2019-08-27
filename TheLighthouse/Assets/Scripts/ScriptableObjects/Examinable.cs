aab3cbff45ceb134fe009bbdb737931cfd55c548
Keaton de Jager <keatondejager@gmail.com>
2019-08-27 17:29:54 +0200

    Setup the prefabs folder to allow for easy importing of the player object.

---
+++ TheLighthouse/Assets/Scripts/ScriptableObjects/Examinable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Examinable Object", menuName = "Examinable Object")]
+public class Examinable : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Transform mesh;
+    public Vector3 originalPosition;
+    public Vector3 originalRotation;
+    
+}
---
+++ TheLighthouse/Assets/Scripts/ScriptableObjects/Searchable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Searchable Object", menuName = "Searchable Object")]
+public class Searchable : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public List<Item> inventory;
+}
