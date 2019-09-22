05554fec93521c2181156ba38cd205844136c82c
Devan <dbg1709@gmail.com>
2019-09-22 16:44:53 +0200

    Built the basic core puzzle of level 1 - in the prototype level in the old unity version.

---
+++ TheLighthouse/Assets/Scripts/Devan's Scripts/CircuitBoardItem3.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitBoardItem3 : MonoBehaviour
+{
+    private bool OnItem = false;
+    bool doorOpened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("On Item");
+            OnItem = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("On Item");
+            OnItem = false;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnItem)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                if (GameObject.Find("Inv").GetComponent<PlayerInventory>().myInventory[2] == true)
+                {
+                    GameObject.Find("Item3InWall").GetComponent<MeshRenderer>().enabled = true;
+                }
+            }
+        }
+
+
+        if (GameObject.Find("Item1InWall").GetComponent<MeshRenderer>().enabled && GameObject.Find("Item2InWall").GetComponent<MeshRenderer>().enabled && GameObject.Find("Item3InWall").GetComponent<MeshRenderer>().enabled)
+        {
+            if(doorOpened == false)
+            {
+                doorOpened = true;
+                GameObject.Find("EXIT DOOR").transform.Rotate(0, 90, 0);
+            }
+            
+        }
+    }
+}
