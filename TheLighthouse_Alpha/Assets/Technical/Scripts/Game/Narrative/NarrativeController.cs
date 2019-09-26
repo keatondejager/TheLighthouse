ee58d2124962cd051410488573dde3ddff359a60
Keaton de Jager <keatondejager@gmail.com>
2019-09-26 10:50:42 +0200

    Started narrative controller

---
+++ TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Narrative/NarrativeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class NarrativeController : MonoBehaviour
+{
+    [SerializeField] protected List<NarrativeObject> Story;
+    [SerializeField] protected AudioSource VoiceLineSource;
+    [SerializeField] protected TMP_Text subtitleSource;
+
+    [SerializeField] protected int storyIndex;
+
+}
