9528cf6269193b5333b575fbc0d829f60c5fd492
Keaton de Jager <keatondejager@gmail.com>
2019-09-28 11:48:08 +0200

    Added a way to read all inputs to be used

--- TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Player/Player States/Definitions/Puzzle.cs
+++ TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Player/Player States/Definitions/Puzzle.cs
@@ -15,13 +15,41 @@ namespace Player
         private delegate void PuzzleInput();
 
         private List<PuzzleInput> InputMethods;
-        
+
+        public enum InputTypes {
+            AnalogueClockwise = 0, AnalogueAntiClockwise = 1,
+            RightTrigger = 2, LeftTrigger = 3,
+            SecondaryButton = 4, TertiaryButton = 5,
+            LeftAnalogueMove = 5, RightAnalogueMove = 6
+        }
+
+        [SerializeField] private Vector2 LeftAnalogueInput;
+        [SerializeField] private Vector2 RightAnalogueInput;
+
+        [SerializeField] private bool isRightTriggerDown = false;
+        [SerializeField] private bool isLeftTriggerDown = false;
+
         public override void Initialize (PlayerInputActions _controls) {
             controls = _controls;
 
             PlayerObject = PlayerReference.instance.manager;
             controls.PuzzleControls.Close.performed += ctx => ExitState();
             controls.PuzzleControls.PrimaryButton.performed += ctx => StartInput();
+
+            controls.PuzzleControls.PrimaryAxis.performed += ctx => LeftAnalogueInput = ctx.ReadValue<Vector2>();
+            controls.PuzzleControls.PrimaryAxis.canceled += ctx => LeftAnalogueInput = Vector2.zero;
+
+            controls.PuzzleControls.SecondaryAxis.performed += ctx => RightAnalogueInput = ctx.ReadValue<Vector2>();
+            controls.PuzzleControls.SecondaryAxis.canceled += ctx => RightAnalogueInput = Vector2.zero;
+
+            controls.PuzzleControls.SecondaryButton.performed += ctx => Secondary();
+            controls.PuzzleControls.TertiaryButton.performed += ctx => Tertiary();
+
+            controls.PuzzleControls.Hold.performed += ctx => isLeftTriggerDown = true;
+            controls.PuzzleControls.Hold.canceled += ctx => isLeftTriggerDown = false;
+
+            controls.PuzzleControls.Grab.performed += ctx => isRightTriggerDown = true;
+            controls.PuzzleControls.Grab.canceled += ctx => isRightTriggerDown = false;
             
         }
 
@@ -30,17 +58,28 @@ namespace Player
         }   
         
         private void StartInput () {
-            // if (checkingInput) {
-            //     return;
-            // }
-
-            // checkingInput = true; 
+            if (checkingInput) {
+                return;
+            }
 
             if (puzzle.puzzleState == 7) {
                 return;
             }
 
-            InputMethods[puzzle.puzzleState].Invoke();
+            checkingInput = true; 
+            // Determine what input is to be expected
+
+            //Determine what action will play 
+
+            //Set the result if action completed correctly            
+        }
+
+        private void Tertiary() {
+            Debug.Log("Tertiary");
+        }
+
+        private void Secondary() {
+            Debug.Log("Secondary");
         }
 
         public override void EnableState() {
