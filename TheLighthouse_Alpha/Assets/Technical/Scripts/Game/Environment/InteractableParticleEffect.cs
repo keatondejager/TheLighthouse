44dac4668f2e02151368adf923a5fd0e3ef4a02b
Matthew Morris <m.morris9491@gmail.com>
2019-09-30 19:24:05 +0200

    Added interactable object particle effect

---
+++ TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Environment/InteractableParticleEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableParticleEffect : MonoBehaviour
+{
+    public ParticleSystem PS;
+
+    private void Awake()
+    {
+        var sh = PS.shape;
+        sh.enabled = true;
+        sh.shapeType = ParticleSystemShapeType.Mesh;
+        sh.mesh = this.transform.parent.GetComponent<MeshFilter>().mesh;
+        sh.scale = this.transform.parent.transform.localScale * 1.5f;
+    }
+}
