cf8cc6b4652a6e78822f61af3b8efceef48b4eb9
Keaton de Jager <keatondejager@gmail.com>
2019-10-06 17:44:31 +0200

    Reworking DB puzzle to make it easy to customize and improve

---
+++ TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Player/Puzzles/DB/DB_State.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DB_State : MonoBehaviour
+{
+    protected Player.PlayerManager manager;
+    protected DistributionBoard puzzleManager;
+
+    protected bool hasRequirement;
+    protected PlayerInputActions controls;
+
+    public virtual void Initialize(DistributionBoard myManager) {
+        manager = Player.PlayerReference.instance.manager;
+        puzzleManager = myManager;
+        controls = new PlayerInputActions();
+
+        controls.PuzzleControls.Close.performed += ctx => manager.UniversalExit();
+        ControlsSetUp();
+    }
+
+    public virtual void Step () {
+
+    }
+
+    public virtual void ControlsSetUp() {
+        controls.PuzzleControls.PrimaryButton.performed += ctx => Debug.Log("X");
+    }
+
+    public virtual bool CheckRequirement () {
+        if (hasRequirement) {
+            //Check if the player has the required thing
+            return false; // or false
+        }
+
+        return true;
+    }
+
+    private void OnEnable() {
+        controls.PuzzleControls.Enable();    
+    }
+
+    private void OnDisable() {
+        controls.PuzzleControls.Disable();
+    }
+}
