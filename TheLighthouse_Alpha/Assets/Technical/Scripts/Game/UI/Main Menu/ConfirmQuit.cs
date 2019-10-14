1df254e193df0960daab66dc7f2b837552a58159
Keaton de Jager <keatondejager@gmail.com>
2019-10-14 17:13:17 +0200

    Main Menu functions properly now

---
+++ TheLighthouse_Alpha/Assets/Technical/Scripts/Game/UI/Main Menu/ConfirmQuit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConfirmQuit : MonoBehaviour
+{
+    protected PlayerInputActions controls;
+
+    private void Awake() {
+        controls = new PlayerInputActions();
+
+        controls.UINavigation.Select.performed += ctx => QuitGame();
+        controls.UINavigation.Cancel.performed += ctx => CancelQuit();
+    }
+    private void OnEnable() {
+        controls.UINavigation.Enable();
+    }
+
+    public void QuitGame () {
+        Application.Quit();
+    }
+
+    public void CancelQuit() {
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnDisable() {
+        controls.UINavigation.Disable();
+    }
+}
