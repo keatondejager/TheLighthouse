47769502b1bc4a91704c47d98d1c94994d518702
Keaton de Jager <keatondejager@gmail.com>
2019-10-15 15:31:19 +0200

    Debugging PS4 Controller problems

--- TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Player/PlayerManager.cs
+++ TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Player/PlayerManager.cs
@@ -57,21 +57,21 @@ namespace Player
         private void Update() {
             state.Step();
 
-            if (Input.GetKeyDown(KeyCode.Alpha1)) {
-                SceneManager.LoadScene(1);
-            }
+            // if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            //     SceneManager.LoadScene(1);
+            // }
 
-            if (Input.GetKeyDown(KeyCode.Alpha2)) {
-                SceneManager.LoadScene(2);
-            }
+            // if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            //     SceneManager.LoadScene(2);
+            // }
 
-            if (Input.GetKeyDown(KeyCode.Alpha3)) {
-                SceneManager.LoadScene(3);
-            }
+            // if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            //     SceneManager.LoadScene(3);
+            // }
 
-            if (Input.GetKeyDown(KeyCode.Alpha4)) {
-                SceneManager.LoadScene(4);
-            }
+            // if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            //     SceneManager.LoadScene(4);
+            // }
         }
 
         public void SetState (States newState) {
