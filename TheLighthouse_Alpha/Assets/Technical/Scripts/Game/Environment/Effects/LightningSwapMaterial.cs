7b4b985fdc5923a1bcf86c1f20e061de964d2dc2
Matthew Morris <m.morris9491@gmail.com>
2019-10-20 12:21:54 +0200

    Created Script to change lightning material swap on lightning particle effect. Also moved all particle sprites and materials into a folder in imported assets

---
+++ TheLighthouse_Alpha/Assets/Technical/Scripts/Game/Environment/Effects/LightningSwapMaterial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningSwapMaterial : MonoBehaviour
+{
+
+    public List<Material> Materials = new List<Material>();
+
+    private ParticleSystem _LightningParticleSystem;
+    private ParticleSystemRenderer _LightningParticleRender;
+    public bool ChangeMaterial = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _LightningParticleSystem = GetComponent<ParticleSystem>();
+        _LightningParticleRender = GetComponent<ParticleSystemRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning(_LightningParticleSystem.particleCount);
+        if(_LightningParticleSystem.particleCount == 0 && ChangeMaterial == true)
+        {
+            _LightningParticleRender.material = Materials[(int)Random.Range(0, Materials.Count)];
+            ChangeMaterial = false;
+        }
+        else if(_LightningParticleSystem.particleCount == 1 && ChangeMaterial == false)
+        {
+            ChangeMaterial = true;
+        }
+    }
+}
